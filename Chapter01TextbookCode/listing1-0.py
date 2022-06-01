72651dfacecd2fc63e0dc588e970242f470364f3
Maggie Burke <msb@alumni.brown.edu>
2022-06-01 07:13:48 -0400

    Add files via upload

---
+++ Chapter01TextbookCode/listing1-0.py
@@ -0,0 +1,10 @@
+import turtle
+
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.circle(50)
---
+++ Chapter01TextbookCode/listing1-1.py
@@ -0,0 +1,12 @@
+import turtle
+
+turtle.penup()
+turtle.setpos(0, -50)
+turtle.pendown()
+turtle.circle(100)
+turtle.left(90)
+turtle.forward(25)
+turtle.penup()
+turtle.forward(150)
+turtle.pendown()
+turtle.forward(25)
